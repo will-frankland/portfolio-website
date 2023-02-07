7bcc7769fcec94587675c720b611b058dd280529
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-07 00:06:03 -0500

    add initial styling and text to Hero section, about me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey, I'm Will <br />
+        Welcome to my portfolio!
+      </SectionTitle>
+      <SectionText>
+        I'm looking for Full Stack or Frontend Devloper work. See my projects below!
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
