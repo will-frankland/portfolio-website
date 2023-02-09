f66183ef8ddfacc1c96a03f058137245c18ff0fb
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-09 17:01:53 -0500

    add footer with email and linked social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,25 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:will-frankland@hotmail.com">will-frankland@hotmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href='https://github.com/will-frankland'>
+        <AiFillGithub size='3rem'/>
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/will-frankland/'>
+        <AiFillLinkedin size='3rem'/>
+      </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
