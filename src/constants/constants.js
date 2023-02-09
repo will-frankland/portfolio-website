a79a5944608de8ea36c09c437f2265ee295eebd9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-09 16:44:09 -0500

    update carousel text to reflect my journey

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2015, text: 'Graduated college with Mechanical Engineering Diploma and began work as a Process Engineer', },
+  { year: 2018, text: 'Took a contractor role working with CAD on Royal Navy Submarines', },
+  { year: 2019, text: 'Moved to Toronto, Canada', },
+  { year: 2022, text: 'Completed Web Development Bootcamp with Lighthouse Labs in Toronto', },
+  { year: 2023, text: 'Working on web portfolio', },
 ];
