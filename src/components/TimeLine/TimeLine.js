950b2a2282fe26667fdc044823f95c17d822a282
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-09 16:39:08 -0500

    add carousel buttons with properties

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -120,6 +120,19 @@ const Timeline = () => {
           ))}
         </>
       </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => (
+          <CarouselButton 
+            key={index}
+            index={index}
+            active={activeItem}
+            onClick={(e) => handleClick(e, index)}
+            type="button"
+          >
+            <CarouselButtonDot active={activeItem}/>
+          </CarouselButton>
+        ))}
+      </CarouselButtons>
     </Section>
   );
 };
